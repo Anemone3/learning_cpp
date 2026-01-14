37889ba198c4efd0a42e25010c2e0cdd2181122f
chocomint <castaneda.jair.13@gmail.com>
2026-01-13 19:26:53 -0500

    axd

---
+++ basic/2_flow_controls.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int y;
+    std::cout << "print some number" << std::endl;
+    std::cin >> y;
+    std::cout << "the number is " << y << std::endl;
+    return 0;
+}
